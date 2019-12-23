3dbebd2bf5a9e574c95d3aebe750fa1fea275b34
anonlatte <biba.slow15@gmail.com>
2019-12-23 20:04:30 +0900

    Opening webview page which depending by language

--- app/src/main/java/com/example/testgame/WebviewActivity.kt
+++ app/src/main/java/com/example/testgame/WebviewActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.webkit.CookieManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_webview.*
+import java.util.*
 
 class WebviewActivity : AppCompatActivity() {
 
@@ -12,7 +13,28 @@ class WebviewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_webview)
 
         CookieManager.getInstance().setAcceptCookie(true)
-        webView.loadUrl("https://www.google.com/")
-    }
 
+        var googleUrl = "https://www.google"
+
+        when (Locale.getDefault()) {
+            Locale.US, Locale.UK, Locale.ENGLISH -> {
+                webView.loadUrl("$googleUrl.com/")
+            }
+            Locale.JAPANESE, Locale.JAPAN -> {
+                webView.loadUrl("$googleUrl.jp/")
+            }
+            Locale.CANADA -> {
+                webView.loadUrl("$googleUrl.ca/")
+            }
+            Locale.CHINA, Locale.CHINESE, Locale.SIMPLIFIED_CHINESE, Locale.TRADITIONAL_CHINESE -> {
+                webView.loadUrl("$googleUrl.cn/")
+            }
+            Locale("ru", "RU") -> {
+                webView.loadUrl("https://yandex.ru/")
+            }
+            else -> {
+                webView.loadUrl("$googleUrl.com/")
+            }
+        }
+    }
 }
